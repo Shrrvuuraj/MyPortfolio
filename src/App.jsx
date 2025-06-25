342558cd187bdd6535e49ca48fe22a383d5f8b21
Shravan Rajput <shrrvvuurajput@gmail.com>
2025-06-25 22:45:35 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,20 @@
-import About from "./section/About"
-import Experiences from "./section/Experiences"
+import About from "./section/About";
+import Experiences from "./section/Experiences";
 
-import Hero from "./section/Hero"
-import Navbar from "./section/Navbar"
-import Projects from "./section/Projects"
-
-
+import Hero from "./section/Hero";
+import Navbar from "./section/Navbar";
+import Projects from "./section/Projects";
+import Testimonial from "./section/Testimonial";
 
 const App = () => {
   return (
-    <div className='container mx-auto max-w-7xl'>
-      <Navbar/>
-      <Hero/>
-      <About/>
-      <Projects/>
-      <Experiences/>
-      
-      
-      <section className="min-h-screen"></section>
-      <section className="min-h-screen"></section>
+    <div className="container mx-auto max-w-7xl">
+      <Navbar />
+      <Hero />
+      <About />
+      <Projects />
+      <Experiences />
+      <Testimonial /> <section className="min-h-screen"></section>
       <section className="min-h-screen"></section>
       <section className="min-h-screen"></section>
       <section className="min-h-screen"></section>
@@ -28,7 +24,7 @@ const App = () => {
       <section className="min-h-screen"></section>
       <section className="min-h-screen"></section>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/section/Testimonial.jsx
+++ src/section/Testimonial.jsx
@@ -1,43 +1,6 @@
 import { Marquee } from "../components/Marquee";
 import { twMerge } from "tailwind-merge";
-const reviews = [
-  {
-    name: "Jack",
-    username: "@jack",
-    body: "I've never seen anything like this before. It's amazing. I love it.",
-    img: "https://avatar.vercel.sh/jack",
-  },
-  {
-    name: "Jill",
-    username: "@jill",
-    body: "I don't know what to say. I'm speechless. This is amazing.",
-    img: "https://avatar.vercel.sh/jill",
-  },
-  {
-    name: "John",
-    username: "@john",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/john",
-  },
-  {
-    name: "Jane",
-    username: "@jane",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/jane",
-  },
-  {
-    name: "Jenny",
-    username: "@jenny",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/jenny",
-  },
-  {
-    name: "James",
-    username: "@james",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/james",
-  },
-];
+import { reviews } from "../constant";
 
 const firstRow = reviews.slice(0, reviews.length / 2);
 const secondRow = reviews.slice(reviews.length / 2);
@@ -52,10 +15,9 @@ const ReviewCard = ({
     <figure
       className={twMerge(
         "relative h-full w-64 cursor-pointer overflow-hidden rounded-xl border p-4",
-        // light styles
-        "border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]",
-        // dark styles
-        "dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]",
+        
+        
+        "border-gray-50/[.1] bg-gradient-to-r bg-indigo to-storm hover:bg-royal hover-animation",
       )}
     >
       <div className="flex flex-row items-center gap-2">
@@ -72,7 +34,7 @@ const ReviewCard = ({
   );
 };
 
-export function MarqueeDemo() {
+export default function Testimonial() {
   return (
     <div className="relative flex w-full flex-col items-center justify-center overflow-hidden">
       <Marquee pauseOnHover className="[--duration:20s]">
